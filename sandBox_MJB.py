fcc768264808379a5d5c8220966cec89bf599dac
Josh Mehlman <eel.josh@gmail.com>
2025-06-03 17:04:32 -0700

    Day 1

--- sandBox_MJB.py
+++ sandBox_MJB.py
@@ -1,3 +1,14 @@
-stringVar = "this is a string"
-intVar = 6
-print(f"Hi mom! {stringVar}:{intVar}")
+squares = [1 , 2, 5, 77]
+print(squares)
+foo = squares
+print(f"foo {foo}")
+#squares = 44
+squares[3] = 44
+#squares[3] = 4
+print(f"foo: {type(foo)} {foo}")
+
+def foo(bar:int):
+    print(bar)
+
+
+foo(5)
