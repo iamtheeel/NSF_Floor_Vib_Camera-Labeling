a77bc45155fa4d2f59a7be12e940f7bcfa839cca
NotYourAverageJack <jscapito@outlook.com>
2025-06-09 12:27:40 -0700

    lunch break 6/9/25, beginning work on distance tracking through edge detection

--- Jack/edgeDetectSB_JSC.py
+++ Jack/edgeDetectSB_JSC.py
@@ -1,14 +1,14 @@
 ####
-#Jack Capito
-#STARS Summer 2025
-#Dr J Lab
+#   Jack Capito
+#   STARS Summer 2025
+#   Dr J Lab
 ###
-#opens a video file in CV2
+# Edgetracing experiment
 ####
 
 import cv2 # pip install opencv-python
 import time
-import pytesseract # pip install pytesseract
+import numpy as np
 
 dir = r'C:\Users\notyo\Documents\STARS\StudentData\25_06_03\Subject_2'
 file = 's2_B8A44FC4B25F_6-3-2025_4-00-20 PM.asf'
@@ -21,24 +21,21 @@ if not videoObject.isOpened():
     exit()
 print(f"Loaded: {filename}")
 
-def nothingburger(text, videoPeram):
+def perameters(text, videoPeram):
     output = videoObject.get(videoPeram)
     print(f"Loaded: {output}{text}")
     return output
 
-fps = nothingburger("fps", cv2.CAP_PROP_FPS)
-vidHeight = nothingburger("px height", cv2.CAP_PROP_FRAME_HEIGHT)
-vidWidth = nothingburger("px width", cv2.CAP_PROP_FRAME_WIDTH)
-frameCount = nothingburger(" frames in frameCount", cv2.CAP_PROP_FRAME_COUNT)
-zoom = nothingburger(" zoom", cv2.CAP_PROP_ZOOM)
-pan = nothingburger(" pan", cv2.CAP_PROP_PAN)
-tilt = nothingburger(" tilt", cv2.CAP_PROP_TILT)
+fps = perameters("fps", cv2.CAP_PROP_FPS)
+vidHeight = perameters("px height", cv2.CAP_PROP_FRAME_HEIGHT)
+vidWidth = perameters("px width", cv2.CAP_PROP_FRAME_WIDTH)
+frameCount = perameters(" frames in frameCount", cv2.CAP_PROP_FRAME_COUNT)
 dispFact=2
 displayRez = (int(vidWidth/dispFact), int(vidHeight/dispFact)) # Calculate the display resolution by dividing the width and height by a factor
 
 
 for i in range(int(frameCount)): 
-    print(f"load frame {i}")
+    #print(f"load frame {i}")
     start_time = time.time()  # Start timing
 
     success, frame = videoObject.read() # .read() returns a boolean value and the frame itself. 
@@ -50,28 +47,18 @@ for i in range(int(frameCount)):
         exit()
 
     
-    lwresframe = cv2.resize(frame, displayRez)
-    #blurred = cv2.GaussianBlur(frame, (31, 31), 0)
-    blackWhite = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    white2Black = cv2.bitwise_not(blackWhite)
+    blackWhite = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    lines = cv2.Canny(blackWhite, 80, 100)
+    lwresframe = cv2.resize(lines, displayRez)
 
-    dateTime_img = white2Black[0:45, 0:384]
-    #print(f"frame: type: {type(dateTime_img)}, len {dateTime_img.shape}")
-    dateTimeOutput = pytesseract.image_to_data(dateTime_img, output_type=pytesseract.Output.DICT)
-    dateTime_str = dateTimeOutput['text']
-    dateTime_conf = dateTimeOutput['conf']
-    print(f"Date: {dateTime_str[4]}, Conf:{dateTime_conf[4]}",
-          f"Time: {dateTime_str[5]}, Conf:{dateTime_conf[5]}") 
-    
-
-    cv2.imshow("Video Frame", dateTime_img) # Display the frame in a window named "Video Frame"
+    cv2.imshow("Video Frame", lwresframe) # Display the frame in a window named "Video Frame"
 
 
 
     processing_time = (time.time() - start_time) * 1000  # in milliseconds
     delay = max(int(1000/fps) - int(processing_time), 1)  # Ensure at least 1 ms delay
 
-    print(f"Processing time: {processing_time:.2f} ms, Delay: {delay} ms")  # Optional: see timing info
+    #print(f"Processing time: {processing_time:.2f} ms, Delay: {delay} ms")  # Optional: see timing info
 
     # stop the clock
     key = cv2.waitKey(delay) # Wait for a key press for a duration based on the video's FPS
